bb41f343d3d4268b134e9ff890139d04fadb4f46
rembertoS <rsilv108@fiu.edu>
2025-01-21 15:39:42 -0500

    explore and  visualize the data

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
+#Generate a dataset
+
+
 data = {
     "Hours_studied": np.random.randint(1 ,10 ,100),
     "Attendace":  np.random.randint(60 , 100, 100),
@@ -13,4 +16,22 @@ data = {
     "Exam_Score": None,
 }
 
+
 data = pd.DateFrame(data)
+data["Exam_Score"] = (
+    5 * data["Hours_Studied"] + 0.5 * data["Extracurricular"] + np.random.normal(0, 5, 100)
+)
+
+
+# Explore the Dataset
+
+print("\nFirst 5 rows of the dataset: ")
+print(data.head())
+print("\nDataset statistics: ")
+print(data.describe())
+
+#Visualize the Data
+
+sns.pairplot(data, diag_kind="kde")
+plt.show()
+
